9f29da687ad16aa05c5e37d47edfa93c6cc9705f
Andrey Popp <8mayday@gmail.com>
2016-04-30 13:08:52 +0300

    style: fix lint error

--- src/register.js
+++ src/register.js
@@ -8,6 +8,6 @@ import {compile} from './index';
 function loadTestdoc(module, filename) {
   let content = fs.readFileSync(filename, 'utf8');
   return module._compile(compile(content, {name: filename}), filename);
-};
+}
 
 require.extensions['.md'] = loadTestdoc;
